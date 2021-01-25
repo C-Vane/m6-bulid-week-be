5bb96150fba21698a97173f2654f3a4126bf2303
rakkas95 <71468575+rakkas95@users.noreply.github.com>
2021-01-25 15:21:41 +0000

    Update schema.js
    experience schema created without functions

---
+++ src/services/Experiences/schema.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+const ExperienceSchema = new Schema(
+  {
+    role: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        required: false,
+    }, 
+    description: String,
+    area: String,
+    image: String,
+    username: [
+        {userId: Schema.Types.ObjectId, username: String, password: String},
+    ],
+
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const ExperienceModel = model("Experience", UserSchema);
+
+module.exports = ExperienceModel;
