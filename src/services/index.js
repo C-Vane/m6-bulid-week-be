b1c21b9b899cfe4d98026ca552055eee92b65517
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2021-01-25 17:43:16 +0100

    fix bug

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const comments = require("./Comments/index.js");
+const post = require("./Post/index.js");
 ////ALL APIS
 router.use("/comments", comments);
+router.use("/post", post);
 module.exports = router;
