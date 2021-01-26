23c873c01a853fed3d7fa330f4ba89a3438a6a77
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2021-01-26 10:36:12 +0100

    fix schema User

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const comments = require("./Comments/index.js");
-const post = require("./Post/index.js");
+const post = require("./Posts/index.js");
 ////ALL APIS
 router.use("/comments", comments);
 router.use("/post", post);
